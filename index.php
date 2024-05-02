4c139ba961912c5fabb4f60ee8d3d9215fc46ebe
Daniele206 <danieledeieso2004@gmail.com>
2024-05-02 16:27:01 +0200

    [add] object film and var_dump

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
+  require_once __DIR__ . '/modell/movie.php';
 
+  $film1 = new Movie('film1' , 'autore1', 'attore1');
+  $film2 = new Movie('film2' , 'autore2', 'attore2');
+
+  var_dump($film1);
+  var_dump($film1->getNameAndAutor());
+  
+  var_dump($film2);
+  var_dump($film2->getNameAndAutor());
 
 ?>
 
@@ -9,9 +18,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP-OOP-1</title>
 </head>
 <body>
-  
+  <h1>PHP-OOP-1</h1>
 </body>
 </html>
