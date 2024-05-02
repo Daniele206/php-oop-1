858af29684d3953934a3d786b76951a0671b0932
Daniele206 <danieledeieso2004@gmail.com>
2024-05-02 16:04:50 +0200

    [create] repo, [add] class Moviel, [add] construct

---
+++ modell/movie.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Movie{
+  public $name;
+  public $autor;
+  public $actor;
+
+  public function __construct(string $_name, string $_autor, string $_actor)
+  {
+    $this.name = $_name;
+    $this.autor = $_autor;
+    $this.actor = $_actor
+  }
+}
