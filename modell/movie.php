f83f600d48b6cae05e99e1366d9db0ecac6e2259
Daniele206 <danieledeieso2004@gmail.com>
2024-05-02 16:11:30 +0200

    [add] method

--- modell/movie.php
+++ modell/movie.php
@@ -7,8 +7,12 @@ class Movie{
 
   public function __construct(string $_name, string $_autor, string $_actor)
   {
-    $this.name = $_name;
-    $this.autor = $_autor;
-    $this.actor = $_actor
+    $this->name = $_name;
+    $this->autor = $_autor;
+    $this->actor = $_actor;
+  }
+
+  public function getNameAndAutor(){
+    return "$this->name creato da $this->autor";
   }
 }
